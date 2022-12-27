6ab3e47ad185b55af3e02db6fb2359c81ff565cf
Tugus <idabgusindra01@gmail.com>
2022-12-27 17:30:48 +0800

    first commit and add reacct-router, react-bootstrap, axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/admin-login' element={<Login />}/>
+    </Routes>
   );
 }
 
---
+++ src/Pages/Login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (  
+        <div>
+            <h1>testinglogin</h1>
+        </div>
+    );
+}
+ 
+export default Login;
